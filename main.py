3b8c58b35e3e41b817e849ba2a243ea7896b5e07
MrDoguito <gabrielgatto13@gmail.com>
2025-11-26 20:34:19 -0300

    Strings to Constant

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from downloader import baixar_legenda
 from cleaner import limpar_vtt
 from summarizer import resumir_texto, nome_arquivo
 import os
-caminho_saida_txt = "clean_subtitles/legenda_limpa.txt"
-caminho_vtt = "subtitles_extracted/video_temp.pt.vtt"
+
+# ==========================
+#         CONSTANTES
+# ==========================
+
+CAMINHO_SAIDA_TXT = "clean_subtitles/legenda_limpa.txt"
+CAMINHO_VTT = "subtitles_extracted/video_temp.pt.vtt"
+PASTA_SUMMARIES = "summaries"
+ARQUIVO_LIMPO = f"{PASTA_SUMMARIES}/{nome_arquivo}"
+ARQUIVO_TEMP_TXT = "clean_subtitles/legenda_limpa.txt"
+ARQUIVO_TEMP_VTT = "subtitles_extracted/video_temp.pt.vtt"
+
+# ==========================
+#          CÓDIGO
+# ==========================
 
 URL = input("Cole a URL do video do YouTube: ")
 
@@ -11,20 +24,21 @@ print("\n🎥 Baixando legenda...")
 legenda_path = baixar_legenda(URL)
 
 print("🧹 Limpando legenda...")
-print("🛠️ Abrindo arquivo:", caminho_vtt)
+print("🛠️ Abrindo arquivo:", CAMINHO_VTT)
 with open(legenda_path, 'r', encoding='utf-8') as f:
     legenda_raw = f.read()
-legenda_limpa = limpar_vtt(caminho_vtt, caminho_saida_txt)
+
+legenda_limpa = limpar_vtt(CAMINHO_VTT, CAMINHO_SAIDA_TXT)
 
 print("🤖 Resumindo com IA...")
 resumo = resumir_texto()
 
 print("📄 RESUMO:")
-with open(f"summaries/{nome_arquivo}", "r", encoding="utf-8") as f:
+with open(ARQUIVO_LIMPO, "r", encoding="utf-8") as f:
     resumo = f.read()
 print(resumo)
 
-print("\n✅ Resumo salvo na pasta Summaries")
+print("\n✅ Resumo salvo na pasta summaries")
 
-os.remove("clean_subtitles/legenda_limpa.txt")
-os.remove("subtitles_extracted/video_temp.pt.vtt")
+os.remove(ARQUIVO_TEMP_TXT)
+os.remove(ARQUIVO_TEMP_VTT)
